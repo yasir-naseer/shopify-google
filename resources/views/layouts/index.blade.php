8fc6f12c4a581a23288e5b1abbd7edd545160c6e
majidalimughal <mughalmajidali101@outlook.com>
2021-02-14 15:25:51 +0500

    Settings+Google Merchant Creation

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -15,6 +15,23 @@
         </div>
     </div>
 </div>
+@if(config('shopify-app.appbridge_enabled'))
+    <script src="https://unpkg.com/@shopify/app-bridge@0.8.2/index.js"></script>
+    <script src="https://unpkg.com/@shopify/app-bridge@0.8.2/actions.js"></script>
+    <script>
+        var AppBridge = window['app-bridge'];
+        var createApp = AppBridge.default;
 
+        var app = createApp({
+            apiKey: '{{ config('shopify-app.api_key') }}',
+            shopOrigin: '{{ \Illuminate\Support\Facades\Auth::user()->name }}',
+            forceRedirect: true,
+        });
+
+
+    </script>
+
+    @include('shopify-app::partials.flash_messages')
+@endif
 </body>
 </html>
