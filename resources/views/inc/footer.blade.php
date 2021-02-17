7fd30c594e2029043ca7a754b0fc07c4985dbe11
majidalimughal <mughalmajidali101@outlook.com>
2021-02-18 02:16:19 +0500

    Billing+Webhook

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -8,18 +8,18 @@
 {{--    </div>--}}
 {{--</footer>--}}
 
-<!-- @if(config('shopify-app.appbridge_enabled'))
-    <script src="https://unpkg.com/@shopify/app-bridge{{ config('shopify-app.appbridge_version') ? '@'.config('shopify-app.appbridge_version') : '' }}"></script>
-    <script>
-        var AppBridge = window['app-bridge'];
-        var createApp = AppBridge.default;
-        var app = createApp({
-            apiKey: '{{ config('shopify-app.api_key') }}',
-            shopOrigin: '{{ Auth::user()->name }}',
-            forceRedirect: true,
-        });
-    </script>
-@endif -->
+{{--<!-- @if(config('shopify-app.appbridge_enabled'))--}}
+{{--    <script src="https://unpkg.com/@shopify/app-bridge{{ config('shopify-app.appbridge_version') ? '@'.config('shopify-app.appbridge_version') : '' }}"></script>--}}
+{{--    <script>--}}
+{{--        var AppBridge = window['app-bridge'];--}}
+{{--        var createApp = AppBridge.default;--}}
+{{--        var app = createApp({--}}
+{{--            apiKey: '{{ config('shopify-app.api_key') }}',--}}
+{{--            shopOrigin: '{{ Auth::user()->name }}',--}}
+{{--            forceRedirect: true,--}}
+{{--        });--}}
+{{--    </script>--}}
+{{--@endif -->--}}
 
 
 <script src="{{ asset('assets/js/oneui.core.min.js') }}"></script>
