db48c42f9026a72f06ce19f33072b0a1b8412fb2
Majid Ali <mughalmajidali101@outlook.com>
2021-04-09 00:33:01 +0500

    Queue

--- app/Jobs/GoogleUpdateJob.php
+++ app/Jobs/GoogleUpdateJob.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
-class GoogleUpdate implements ShouldQueue
+class GoogleUpdateJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
     public $product,$request;
